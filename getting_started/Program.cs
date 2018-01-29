c3971f6a11518d96a438348f1b846db9fdfe10e4
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-01-29 16:12:20 +0100

    setting up first project

---
+++ getting_started/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace getting_started
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
