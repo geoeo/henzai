6b6ff3a2df52eafe1bcea47d8acd966281164fc2
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-02-10 03:16:50 +0100

    Added GuiRenderable

---
+++ Henzai/GUIOverlay.cs
@@ -0,0 +1,13 @@
+using System;
+using ImGuiNET;
+using Veldrid;
+
+namespace Henzai
+{
+    public class GUIRenderable : IDisposable
+    {
+        public void Dispose(){
+            
+        }
+    }
+}
