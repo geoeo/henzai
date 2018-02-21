5964e32d946054e5fa6a5b5143d51bbd120f0677
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-02-21 17:36:55 +0100

    Minor name changes

--- Henzai/Runtime.cs
+++ Henzai/Runtime.cs
@@ -13,32 +13,32 @@ namespace Henzai.Runtime
         //https://stackoverflow.com/questions/2871/reading-a-c-c-data-structure-in-c-sharp-from-a-byte-array
         public static T ByteArrayToStructure<T>(byte[] bytes) where T: struct 
         {
-            T stuff;
+            T value;
             GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
             try
             {
-                stuff = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+                value = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
             }
             finally
             {
                 handle.Free();
             }
-            return stuff;
+            return value;
         }
 
         // https://social.msdn.microsoft.com/Forums/en-US/f3ac392a-43e4-4688-83bc-8caffc9a4105/convert-struct-to-byte?forum=csharplanguage
         public static byte[] ToBytes<T>(T value) where T : struct  
         {  
             Byte[] bytes = new Byte[Marshal.SizeOf(typeof(T))];  
-            GCHandle pinStructure = GCHandle.Alloc(value, GCHandleType.Pinned); 
+            GCHandle handle = GCHandle.Alloc(value, GCHandleType.Pinned); 
             try 
             {  
-                Marshal.Copy(pinStructure.AddrOfPinnedObject(), bytes, 0, bytes.Length);  
+                Marshal.Copy(handle.AddrOfPinnedObject(), bytes, 0, bytes.Length);  
                 return bytes;  
             }  
             finally 
             {  
-                pinStructure.Free();  
+                handle.Free();  
             }  
         } 
     }
