3ef7b7c8162ae3617abf5b07eaaf7981dc9174a0
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-03-30 14:49:09 +0200

    addded RenderCommandGenerator

---
+++ Henzai/RenderCommandGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.CompilerServices;
+using Veldrid;
+
+namespace Henzai
+{
+    public static class RenderCommandGenerator
+    {
+
+        //VertexPositionNormalTextureTangentBitangent
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void GenerateCommandsForPNTTB(DeviceBuffer vertexBuffer, DeviceBuffer indexBuffer){
+
+        }
+
+    }
+}
