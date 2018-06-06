c8f9bcecd572352a6b579cc26934c8788743fd18
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-06-06 18:06:51 +0200

    Added synchronous random sampling

--- Henzai/RandomSampling.cs
+++ Henzai/RandomSampling.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 // Implementation from https://github.com/mellinoe/veldrid-raytracer/blob/master/src/raytracer/RandUtil.cs
 // Theory from Raytracing in a Weekend
@@ -7,6 +8,11 @@ namespace Henzai.Sampling
 {
     public static class RandomSampling
     {
+        private static Random randState = new Random();
+        private static Random randState_2 = new Random();
+        private static Object lockState = new Object();
+        private static Object lockState_2 = new Object();
+
         public static uint XorShift(ref uint state)
         {
             state ^= state << 13;
@@ -17,7 +23,20 @@ namespace Henzai.Sampling
 
         public static float RandomFloat(ref uint state)
         {
-            return XorShift(ref state) * (1f / 4294967296f);
+            float ret;
+            uint rState = (uint)randState_2.Next();
+            ret = XorShift(ref rState) * (1f / 4294967296f);
+            return ret;
+        }
+
+        public static float RandomFloat_Sync()
+        {
+            float ret;
+            lock(lockState_2){
+                uint state = (uint)randState_2.Next();
+                ret = XorShift(ref state) * (1f / 4294967296f);
+            }
+            return ret;
         }
 
         public static Vector3 RandomInUnitDisk(ref uint state)
@@ -38,6 +57,27 @@ namespace Henzai.Sampling
                 ret = 2f * new Vector3(RandomFloat(ref state), RandomFloat(ref state), RandomFloat(ref state)) - Vector3.One;
             } while (ret.LengthSquared() >= 1f);
             return ret;
+        }
+
+        //http://corysimon.github.io/articles/uniformdistn-on-sphere/
+        public static Vector3 RandomInUnitSphere_Sync()
+        {
+            //var rand = new Random();
+            // float rand1 = RandomFloat(ref state);
+            // float rand2 = RandomFloat(ref state);
+            double rand1;
+            double rand2;
+            lock(lockState){
+                rand1 = randState.NextDouble();
+                rand2 = randState.NextDouble();
+            }
+
+            var theta = 2.0f * MathF.PI * rand1;
+            var phi = MathF.Acos(1.0f - 2.0f * (float)rand2);
+            float x = MathF.Sin(phi) * MathF.Cos((float)theta);
+            float y = MathF.Sin(phi) * MathF.Sin((float)theta);
+            float z = MathF.Cos(phi);
+            return new Vector3(x,y,z);
         }
     }
 }
