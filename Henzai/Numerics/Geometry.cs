518b71a573bee39bc02e8a646c19b5189cca3794
marc <marc.dhaubenstock@gmail.com>
2018-06-22 13:10:14 +0200

    adapted coordniate frame construction

--- Henzai/Numerics/Geometry.cs
+++ Henzai/Numerics/Geometry.cs
@@ -10,36 +10,24 @@ namespace Henzai.Numerics
         /// <summary>
         /// Creates a Right Handed Coordinate System where the Normal defines the Up Vector
         /// https://www.scratchapixel.com/lessons/3d-basic-rendering/global-illumination-path-tracing/global-illumination-path-tracing-practical-implementation
+        /// Peter Shirley's Raytracing Miniseries
         /// </summary>
        //
        public static void CreateCoordinateSystemAroundNormal(ref Vector3 N, ref Vector3 Nt, ref Vector3 Nb){
-            if(MathF.Abs(N.X)  > MathF.Abs(N.Y)){
-                float mag = MathF.Sqrt(N.X*N.X + N.Y*N.Y);
-                Nt.X = N.Z / mag;
-                Nt.Y = 0.0f;
-                Nt.Z = -N.X / mag;
-            } 
-            else {
-                float mag = MathF.Sqrt(N.Z*N.Z + N.Y*N.Y); 
-                Nt.X = 0.0f;
-                Nt.Y = -N.Z / mag;
-                Nt.Z = N.Y / mag;
+            Vector3 a;
+            if (MathF.Abs(N.X) > MathF.Abs(N.Y))
+            {
+                a = Vector3.UnitY;
+            }
+            else
+            {
+                a = Vector3.UnitX;
             }
 
-            Nb = Vector3.Normalize(Vector3.Cross(N,Nt));
-            Nt = Vector3.Normalize(Vector3.Cross(N,Nb));
-            // Vector3 a;
-            // if(MathF.Abs(N.X) > 0.9f){
-            //     a = Vector3.UnitY;
-            // }
-            // else {
-            //     a = Vector3.UnitX;
-            // }
+            Nb = Vector3.Normalize(Vector3.Cross(N, a));
+            Nt = Vector3.Normalize(Vector3.Cross(N, Nb));
 
-            // Nb = Vector3.Normalize(Vector3.Cross(N,a));
-            // Nt = Vector3.Normalize(Vector3.Cross(N,Nb));
-
-       }
+        }
 
    }
 }
