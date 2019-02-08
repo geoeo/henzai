77e259d551dc8f5b78fb38e20ff2fb57b38960bc
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-02-08 15:10:01 +0100

    syntax

--- Raytracer/Geometry/Ray.fs
+++ Raytracer/Geometry/Ray.fs
@@ -4,13 +4,11 @@ open Raytracer.Geometry.Types
 open Henzai.Core.Numerics
 open System.Numerics
 
+[<Struct>]
 type Ray =
-    struct
         val Origin : Point
         val Direction : Direction
         val SurfaceOrigin : ID
 
         new(origin, dir) = { Origin = origin; Direction = Henzai.Core.Numerics.Vector.Normalize(&dir);SurfaceOrigin = 0UL }
         new(origin, dir, id) = { Origin = origin; Direction = Henzai.Core.Numerics.Vector.Normalize(&dir);SurfaceOrigin = id }
-
-    end
