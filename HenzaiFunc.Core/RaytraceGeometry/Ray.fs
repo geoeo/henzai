ccae213623697d2d36a62cb4c8e146930979ecf6
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-02-13 00:54:24 +0100

    Added readonly

--- HenzaiFunc.Core/RaytraceGeometry/Ray.fs
+++ HenzaiFunc.Core/RaytraceGeometry/Ray.fs
@@ -1,14 +1,14 @@
 namespace HenzaiFunc.Core.RaytraceGeometry
 
 open HenzaiFunc.Core.Types
+open System.Runtime.CompilerServices
 open Henzai.Core.Numerics
-open System.Numerics
 
-[<Struct>]
+[<IsReadOnly;Struct>]
 type Ray =
         val Origin : Point
         val Direction : Direction
         val SurfaceOrigin : ID
 
-        new(origin, dir) = { Origin = origin; Direction = Henzai.Core.Numerics.Vector.Normalize(ref dir);SurfaceOrigin = 0UL }
-        new(origin, dir, id) = { Origin = origin; Direction = Henzai.Core.Numerics.Vector.Normalize(ref dir);SurfaceOrigin = id }
+        new(origin, dir) = { Origin = origin; Direction = Vector.Normalize(ref dir);SurfaceOrigin = 0UL }
+        new(origin, dir, id) = { Origin = origin; Direction = Vector.Normalize(ref dir);SurfaceOrigin = id }
