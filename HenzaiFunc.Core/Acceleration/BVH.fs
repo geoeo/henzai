bed35d9931f60f5b0b122e19efe3a06749663862
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-02-13 22:11:30 +0100

    implemented BVH contruction with middle split

--- HenzaiFunc.Core/Acceleration/BVH.fs
+++ HenzaiFunc.Core/Acceleration/BVH.fs
@@ -17,10 +17,11 @@ module BVHTree =
     let calculateSplitPoint (primitiveSubArray : BVHPrimitive []) (start : int) (finish : int) (centroidBounds : AABB) (dim : SplitAxis) (splitMethod : SplitMethods) =
         match splitMethod with
         | SplitMethods.Middle ->
-            let midFloat = accessPointBySplitDim centroidBounds.PMin dim + accessPointBySplitDim centroidBounds.PMax dim / 2.0f
+            let midFloat = (accessPointBySplitDim centroidBounds.PMin dim + accessPointBySplitDim centroidBounds.PMax dim) / 2.0f
+            //TODO: investigate in place solution without allocating
             let smaller, larger = Array.partition (fun (elem : BVHPrimitive) -> accessPointBySplitDim (AABB.center elem.aabb) dim < midFloat) primitiveSubArray
             let mid = smaller.Length
-            (mid , [])
+            (mid , smaller, larger)
         | x -> failwithf "Recursive splitmethod %u not yet implemented" (LanguagePrimitives.EnumToValue x)
     
     let decomposeBVHBuild tree = 
@@ -39,7 +40,7 @@ module BVHTree =
         else            
             // TODO: investigate Span type for this when upgrading to >= F#4.5
             // TODO: profile this
-            let subArray = Array.sub primitiveArray start nPrimitives
+            let subArray = primitiveArray.[start..finish-1]
             let centroidBounds = Array.fold (fun acc (elem : BVHPrimitive) -> AABB.unionWithPoint acc (AABB.center elem.aabb)) (AABB()) subArray
             let dim = AABB.maximumExtent centroidBounds
             // Very rare case
@@ -49,14 +50,16 @@ module BVHTree =
                 let leaf = Node (BVHNode(SplitAxis.None, orderedGeometryList.Length, nPrimitives, bounds), Empty, Empty)
                 (leaf, newOrderedList)
             else
-                let splitPoint , orderedGeometryList = calculateSplitPoint subArray start finish centroidBounds dim splitMethod
+                let splitPoint , smallerThanMidArray, largerThanMidArray = calculateSplitPoint subArray start finish centroidBounds dim splitMethod
+                Array.blit smallerThanMidArray 0 primitiveArray start smallerThanMidArray.Length
+                Array.blit largerThanMidArray 0 primitiveArray (start + smallerThanMidArray.Length) largerThanMidArray.Length
                 // TODO: investigate Async
-                let (leftSubTree, leftSubList) = recursiveBuild geometryArray primitiveArray start splitPoint orderedGeometryList splitMethod
-                let (rightSubTree, rightSubList) = recursiveBuild geometryArray primitiveArray splitPoint finish orderedGeometryList splitMethod
+                let (leftSubTree, leftOrderedSubList) = recursiveBuild geometryArray primitiveArray start splitPoint orderedGeometryList splitMethod
+                let (rightSubTree, rightOrderedSubList) = recursiveBuild geometryArray primitiveArray splitPoint finish orderedGeometryList splitMethod
                 let leftNode, ll, lr =  decomposeBVHBuild leftSubTree
                 let rightNode, rl, rr =  decomposeBVHBuild rightSubTree
                 let bvhNode = BVHNode(dim, start, nPrimitives, AABB.unionWithAABB leftNode.aabb rightNode.aabb)
-                (Node (bvhNode, Node (leftNode , ll, lr), Node (rightNode , rl, rr)), List.concat [leftSubList; rightSubList])
+                (Node (bvhNode, Node (leftNode , ll, lr), Node (rightNode , rl, rr)), List.concat [leftOrderedSubList; rightOrderedSubList])
 
     let build ( geometryArray : AxisAlignedBoundable []) (primitiveArray : BVHPrimitive []) (splitMethod : SplitMethods) = 
         match splitMethod with
