ead32e43f71405e379d3f5b18e35acf06e1b3f74
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-02-16 02:17:10 +0100

    added large BVH test; seems to be working fine

--- HenzaiFunc.Test/BVHTests.fs
+++ HenzaiFunc.Test/BVHTests.fs
@@ -19,6 +19,15 @@ let standardSphereGeomertryArray : AxisAlignedBoundable []  =
         Sphere(Vector3(2.0f,-4.5f,-7.0f),1.5f)
     |]
 
+let largeSphereGeomertryArray : AxisAlignedBoundable []  = 
+    [|
+       Sphere(Vector3(4.0f,-2.0f,-16.0f),3.5f);
+       Sphere(Vector3(4.0f,5.0f,-16.5f),3.5f);
+       Sphere(Vector3(4.0f,-1.0f,-15.0f),3.5f);
+       Sphere(Vector3(5.0f,-4.5f,-7.0f),1.5f);
+       Sphere(Vector3(2.0f,-4.5f,-7.0f),1.5f)
+    |]
+
 let overlappingSphereGeomertryArray : AxisAlignedBoundable []  = 
     [|
      Sphere(Vector3(4.0f,-1.0f,-15.0f),100.0f);
@@ -58,6 +67,12 @@ let buildBVHIdenticalSphereTest () =
     Assert.Equal(3, orderedPrimitiveList.Length)
 
 [<Fact>]
+let buildBVHLargeSphereTest () =
+    let (bvhTree, orderedPrimitiveList, nodeCount) = BVHTree.build largeSphereGeomertryArray SplitMethods.Middle
+    Assert.Equal(9, nodeCount)
+    Assert.Equal(5, orderedPrimitiveList.Length)
+
+[<Fact>]
 let flattenBVHSimpleSphereTest () =
     let (bvhTree, orderedPrimitiveList, nodeCount) = BVHTree.build simpleSphereGeomertryArray SplitMethods.Middle
     let bvhArray = BVHRuntime.allocateMemoryForBVHRuntime nodeCount
@@ -66,10 +81,60 @@ let flattenBVHSimpleSphereTest () =
     let leafNode = bvhRuntimeNode.leafNode 
     let primitive = simpleSphereGeomertryArray.[leafNode.primitivesOffset]
     let sphere = primitive :?> Sphere
+
     Assert.Equal(Vector3(4.0f,-1.0f,-15.0f), sphere.Center)
     Assert.Equal(1, nodeCount)
 
 
+[<Fact>]
+let flattenBVHIdenticalSphereTest () =
+    let (bvhTree, orderedPrimitiveArray, nodeCount) = BVHTree.build identicalSphereGeomertryArray SplitMethods.Middle
+    let bvhArray = BVHRuntime.allocateMemoryForBVHRuntime nodeCount
+    let out = BVHRuntime.flattenBVHTree bvhTree bvhArray 0 CoordinateSystem.XYNegZ
+    let bvhRuntimeNode_Zero = bvhArray.[0]
+    let leafNode_Zero = bvhRuntimeNode_Zero.leafNode
+    let primitive_Zero = orderedPrimitiveArray.[leafNode_Zero.primitivesOffset]
+    let sphere_Zero = primitive_Zero :?> Sphere
+
+    Assert.Equal(1, nodeCount)
+    Assert.Equal(bvhRuntimeNode_Zero.nPrimitives, 3)
+    Assert.Equal(Vector3(2.0f,-4.5f,-7.0f), sphere_Zero.Center)
+
+
+        
+[<Fact>]
+let flattenBVHOverlappingSphereTest () =
+    let (bvhTree, orderedPrimitiveArray, nodeCount) = BVHTree.build overlappingSphereGeomertryArray SplitMethods.Middle
+    let bvhArray = BVHRuntime.allocateMemoryForBVHRuntime nodeCount
+    let out = BVHRuntime.flattenBVHTree bvhTree bvhArray 0 CoordinateSystem.XYNegZ
+    let bvhRuntimeNode_Zero = bvhArray.[0]
+    let bvhRuntimeNode_One = bvhArray.[1]
+    let bvhRuntimeNode_Two = bvhArray.[2]
+    let bvhRuntimeNode_Three = bvhArray.[3]
+    let bvhRuntimeNode_Four = bvhArray.[4]
+    let leafNode_Zero = bvhRuntimeNode_Two.leafNode
+    let leafNode_One = bvhRuntimeNode_Three.leafNode
+    let leafNode_Two = bvhRuntimeNode_Four.leafNode
+    let primitive_Zero = orderedPrimitiveArray.[leafNode_Zero.primitivesOffset]
+    let primitive_One = orderedPrimitiveArray.[leafNode_One.primitivesOffset]
+    let primitive_Two = orderedPrimitiveArray.[leafNode_Two.primitivesOffset]
+    let sphere_Zero = primitive_Zero :?> Sphere
+    let sphere_One = primitive_One :?> Sphere
+    let sphere_Two = primitive_Two :?> Sphere
+
+    Assert.Equal(5, nodeCount)
+
+    Assert.Equal(bvhRuntimeNode_Zero.nPrimitives, 0)
+    Assert.Equal(bvhRuntimeNode_One.nPrimitives, 0)
+    Assert.Equal(bvhRuntimeNode_Two.nPrimitives, 1)
+    Assert.Equal(bvhRuntimeNode_Three.nPrimitives, 1)
+    Assert.Equal(bvhRuntimeNode_Four.nPrimitives, 1)
+
+    Assert.Equal(Vector3(2.0f,-4.5f,-7.0f), sphere_Zero.Center)
+    Assert.Equal(Vector3(2.5f,-4.5f,-7.0f), sphere_One.Center)  
+    Assert.Equal(Vector3(4.0f,-1.0f,-15.0f), sphere_Two.Center)
+    
+    
 [<Fact>]
 let flattenBVHSphereTest () =
     let (bvhTree, orderedPrimitiveArray, nodeCount) = BVHTree.build standardSphereGeomertryArray SplitMethods.Middle
@@ -89,6 +154,7 @@ let flattenBVHSphereTest () =
     let sphere_Zero = primitive_Zero :?> Sphere
     let sphere_One = primitive_One :?> Sphere
     let sphere_Two = primitive_Two :?> Sphere
+
     Assert.Equal(5, nodeCount)
 
     Assert.Equal(bvhRuntimeNode_Zero.nPrimitives, 0)
@@ -100,6 +166,59 @@ let flattenBVHSphereTest () =
     Assert.Equal(Vector3(2.0f,-4.5f,-7.0f), sphere_Zero.Center)
     Assert.Equal(Vector3(10.0f,-4.5f,-7.0f), sphere_One.Center)  
     Assert.Equal(Vector3(4.0f,-1.0f,-15.0f), sphere_Two.Center)
+
+
+[<Fact>]
+let flattenBVHLargeSphereTest () =
+    let (bvhTree, orderedPrimitiveArray, nodeCount) = BVHTree.build largeSphereGeomertryArray SplitMethods.Middle
+    let bvhArray = BVHRuntime.allocateMemoryForBVHRuntime nodeCount
+    let out = BVHRuntime.flattenBVHTree bvhTree bvhArray 0 CoordinateSystem.XYNegZ
+
+    let bvhRuntimeNode_Zero = bvhArray.[0]
+    let bvhRuntimeNode_One = bvhArray.[1]
+    let bvhRuntimeNode_Two = bvhArray.[2]
+    let bvhRuntimeNode_Three = bvhArray.[3]
+    let bvhRuntimeNode_Four = bvhArray.[4]
+    let bvhRuntimeNode_Five = bvhArray.[5]
+    let bvhRuntimeNode_Six = bvhArray.[6]
+    let bvhRuntimeNode_Seven = bvhArray.[7]
+    let bvhRuntimeNode_Eight = bvhArray.[8]
+
+    let leafNode_Zero = bvhRuntimeNode_Two.leafNode
+    let leafNode_One = bvhRuntimeNode_Three.leafNode
+    let leafNode_Two = bvhRuntimeNode_Six.leafNode
+    let leafNode_Three = bvhRuntimeNode_Seven.leafNode
+    let leafNode_Four = bvhRuntimeNode_Eight.leafNode
+
+    let primitive_Zero = orderedPrimitiveArray.[leafNode_Zero.primitivesOffset]
+    let primitive_One = orderedPrimitiveArray.[leafNode_One.primitivesOffset]
+    let primitive_Two = orderedPrimitiveArray.[leafNode_Two.primitivesOffset]
+    let primitive_Three = orderedPrimitiveArray.[leafNode_Three.primitivesOffset]
+    let primitive_Four = orderedPrimitiveArray.[leafNode_Four.primitivesOffset]
+
+    let sphere_Zero = primitive_Zero :?> Sphere
+    let sphere_One = primitive_One :?> Sphere
+    let sphere_Two = primitive_Two :?> Sphere
+    let sphere_Three = primitive_Three :?> Sphere
+    let sphere_Four = primitive_Four :?> Sphere
+
+    Assert.Equal(9, nodeCount)
+
+    Assert.Equal(bvhRuntimeNode_Zero.nPrimitives, 0)
+    Assert.Equal(bvhRuntimeNode_One.nPrimitives, 0)
+    Assert.Equal(bvhRuntimeNode_Two.nPrimitives, 1)
+    Assert.Equal(bvhRuntimeNode_Three.nPrimitives, 1)
+    Assert.Equal(bvhRuntimeNode_Four.nPrimitives, 0)
+    Assert.Equal(bvhRuntimeNode_Five.nPrimitives, 0)
+    Assert.Equal(bvhRuntimeNode_Six.nPrimitives, 1)
+    Assert.Equal(bvhRuntimeNode_Seven.nPrimitives, 1)
+    Assert.Equal(bvhRuntimeNode_Eight.nPrimitives, 1)
+
+    Assert.Equal(Vector3(2.0f,-4.5f,-7.0f), sphere_Zero.Center)
+    Assert.Equal(Vector3(5.0f,-4.5f,-7.0f), sphere_One.Center)  
+    Assert.Equal(Vector3(4.0f,-1.0f,-15.0f), sphere_Two.Center)
+    Assert.Equal(Vector3(4.0f,-2.0f,-16.0f), sphere_Three.Center)
+    Assert.Equal(Vector3(4.0f,5.0f,-16.5f), sphere_Four.Center)
 
     
 
