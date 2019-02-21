55c4ae555a5781221bdfb1a67056f814c560b32f
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-02-21 22:15:51 +0100

    tests + renaming

--- HenzaiFunc.Test/AABBTest.fs
+++ HenzaiFunc.Test/AABBTest.fs
@@ -1,1 +1,13 @@
-﻿
+﻿module AABBTests
+
+open System.Numerics
+open Xunit
+open HenzaiFunc.Core.RaytraceGeometry
+
+[<Fact>]
+let AABBRayIntersectionTest() = 
+    let aabb = AABB(Vector3(0.0f, 0.0f, -1.0f), Vector3(1.0f, 1.0f, 0.0f))
+    let ray = Ray(Vector3(0.5f, 0.5f, 1.0f), Vector3(0.0f, 0.0f, -1.0f))
+    let (hasIntersection, tHit) = aabb.AsHitable.Intersect ray
+    Assert.True(hasIntersection)
+    
