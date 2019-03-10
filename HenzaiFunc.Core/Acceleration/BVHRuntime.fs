db1285f9a5dd716e5bee2737e51a144066489adb
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-03-10 19:06:55 +0100

    vs fucked up again

---
+++ HenzaiFunc.Core/Acceleration/BVHRuntime.fs
@@ -0,0 +1,121 @@
+﻿namespace HenzaiFunc.Core.Acceleration
+
+open System.Numerics
+open HenzaiFunc.Core.Types
+open HenzaiFunc.Core.RaytraceGeometry
+
+// Phyisically Based Rendering Third Edition p. 280
+/// Implements methods to optimize a BVH BST into a runtime representation
+type BVHRuntime<'T when 'T :> Hitable>() = 
+   
+    let determineSubTreeOrder ((l, r) : (BVHTree * BVHTree)) (splitAxis : SplitAxis) (coordinateSystem : CoordinateSystem) =
+        match splitAxis, coordinateSystem with
+        | SplitAxis.X, CoordinateSystem.XYNegZ -> (l, r)
+        | SplitAxis.Y, CoordinateSystem.XYNegZ -> (l, r)
+        | SplitAxis.Z, CoordinateSystem.XYNegZ -> (r, l)
+        | SplitAxis.X, CoordinateSystem.XYZ -> (l, r)
+        | SplitAxis.Y, CoordinateSystem.XYZ -> (l, r)
+        | SplitAxis.Z, CoordinateSystem.XYZ -> (l, r)
+        | (_, x) -> failwithf "coordinate system %u for subtree not implemented!" (LanguagePrimitives.EnumToValue x)
+
+    let accessBySplitAxis (v1, v2, v3) axis = 
+        match axis with
+        | SplitAxis.X -> v1
+        | SplitAxis.Y -> v2
+        | SplitAxis.Z -> v3
+        | x -> failwithf "Accessed point by %u. Should not happen!" (LanguagePrimitives.EnumToValue x)
+
+    /// Flattens the BVHTree into the supplied bvhRuntimeArray.
+    /// Returns 1. This is an artefact of the C++ implementation it is based on.
+    //TODO: Refactor to make more functional
+    let rec flattenBVHTreeRec (bvhTree : BVHTree) (bvhRuntimeArray : BVHRuntimeNode []) (currentOffset : int) (coordinateSystem : CoordinateSystem) =
+        if bvhRuntimeArray.Length = 0 then
+            (0,0)
+        else
+            let (currentNode, leftSubTree, rightSubTree) = BVHTree.decompose bvhTree
+            let currentBounds = currentNode.aabb
+            let firstChild = currentOffset + 1
+            if currentNode.nPrimitives > 0 then
+                let leafRuntimeNode = LeafRuntimeNode(currentNode.firstPrimitiveOffset)
+                bvhRuntimeArray.[currentOffset] <- BVHRuntimeNode(currentBounds, leafRuntimeNode, currentNode.nPrimitives)
+                (currentOffset, 1)
+            else
+                let splitAxis = currentNode.splitAxis
+                let (closerSubTree, fartherSubTree) = determineSubTreeOrder (leftSubTree, rightSubTree) splitAxis coordinateSystem
+                let (firstChildOffset, count) = 
+                    flattenBVHTreeRec closerSubTree bvhRuntimeArray firstChild coordinateSystem
+
+                let (secondChildOffset, count2) = 
+                    flattenBVHTreeRec fartherSubTree bvhRuntimeArray (firstChildOffset + count) coordinateSystem
+
+                let interiorRuntimeNode = InteriorRuntimeNode(secondChildOffset, splitAxis)
+                bvhRuntimeArray.[currentOffset] <- BVHRuntimeNode(currentBounds, interiorRuntimeNode, 0)
+                //TODO: this is buggy
+                (currentOffset, count + count2 + 1)
+
+    member this.allocateMemoryForBVHRuntime nodeCount = 
+        Array.zeroCreate<BVHRuntimeNode> nodeCount
+
+    /// Allocates memory and flattens the array
+    member this.constructBVHRuntime bvhTree nodeCount =
+        let bvhArray = this.allocateMemoryForBVHRuntime nodeCount
+        flattenBVHTreeRec bvhTree bvhArray 0 CoordinateSystem.XYNegZ |> ignore
+        bvhArray
+
+    member this.flattenBVHTree (bvhTree : BVHTree) (bvhRuntimeArray : BVHRuntimeNode []) (currentOffset : int) (coordinateSystem : CoordinateSystem) = 
+        flattenBVHTreeRec bvhTree bvhRuntimeArray currentOffset coordinateSystem
+
+    /// Finds the closest intersection for the given ray
+    member this.traverse (bvhArray : BVHRuntimeNode []) (orderedPrimitives : 'T []) (nodesToVisit : int[]) (ray : Ray) = 
+        let invDir = Vector3(1.0f / ray.Direction.X, 1.0f/ ray.Direction.Y, 1.0f / ray.Direction.Z)
+        let struct(isXDirNeg, isYDirNeg, isZDirNeg) = struct(invDir.X < 0.0f, invDir.Y < 0.0f, invDir.Z < 0.0f)
+        let mutable toVisitOffset = 0
+        let mutable currentNodeIndex = 0
+        let mutable hasIntersection = false
+        let mutable tHit = System.Single.MaxValue
+        let mutable intersectedGeometry : ('T option) = None
+        
+
+        while toVisitOffset >= 0 do
+            let node = bvhArray.[currentNodeIndex]
+            let id = node.leafNode.primitivesOffset
+            let nPrimitives = node.nPrimitives
+            if node.aabb.AsHitable.HasIntersection ray then
+                // leaf
+                if nPrimitives > 0 then
+                    for i in 0..nPrimitives-1 do
+                        let geometry = orderedPrimitives.[node.leafNode.primitivesOffset + i]
+                        let (leafHasIntersection, leafHit) = geometry.Intersect ray
+                        let point = ray.Origin + leafHit*ray.Direction
+                        let intersectionAcceptable = geometry.IntersectionAcceptable leafHasIntersection leafHit 0.0f point
+                        if intersectionAcceptable && leafHit < tHit then
+                            hasIntersection <- leafHasIntersection
+                            tHit <- leafHit
+                            intersectedGeometry <- Some geometry
+                    toVisitOffset <- toVisitOffset - 1
+                    if toVisitOffset >= 0 then 
+                        currentNodeIndex <- nodesToVisit.[toVisitOffset]
+                // interor node
+                else
+                    let interiorNode = node.interiorNode
+                    if accessBySplitAxis (isXDirNeg, isYDirNeg, isZDirNeg) interiorNode.splitAxis then
+                        nodesToVisit.[toVisitOffset] <- currentNodeIndex + 1
+                        toVisitOffset <- toVisitOffset + 1 
+                        currentNodeIndex <- interiorNode.secondChildOffset
+                    else
+                        nodesToVisit.[toVisitOffset] <- interiorNode.secondChildOffset
+                        toVisitOffset <- toVisitOffset + 1 
+                        currentNodeIndex <- currentNodeIndex + 1
+             else
+                 toVisitOffset <- toVisitOffset - 1
+                 if toVisitOffset >= 0 then 
+                     currentNodeIndex <- nodesToVisit.[toVisitOffset]
+        struct(hasIntersection, tHit, intersectedGeometry)
+
+
+        
+            
+        
+        
+        
+        
---
+++ HenzaiFunc.Core/Extensions/Array.fs
@@ -0,0 +1,69 @@
+﻿namespace HenzaiFunc.Core.Extensions
+
+
+module Array =
+
+    type 'T``[]`` with
+        //TODO: test
+        /// Partitions x such that x[i..k-1] <= x[k] <= x[k+1..j]
+        /// Returns k, the new idx of x[pidx]
+        member x.PartitionInPlace(map : ('T -> 'U)  when 'U : comparison, i: int, j: int, pidx: int) =
+            assert (pidx < j)
+            assert (j < x.Length)
+
+            let p = x.[pidx]
+            let mappedP = map(p)
+            let mutable running = true
+            let mutable smallIdx = i
+            let mutable largeIdx = j
+            let mutable mappedSmall = map(x.[smallIdx])
+            let mutable mappedLarge = map(x.[largeIdx])
+            let mutable k = -1
+
+            while running do
+                while mappedSmall < mappedP && smallIdx < j do
+                    smallIdx <- smallIdx + 1
+                    mappedSmall <- map(x.[smallIdx])
+
+                while mappedLarge > mappedP && largeIdx > i do
+                    largeIdx <- largeIdx - 1
+                    mappedLarge <- map(x.[largeIdx])
+
+                if smallIdx >= largeIdx then
+                    k <- smallIdx
+                    running <- false
+                else
+                    if mappedSmall = mappedP && not (smallIdx = pidx) then
+                        smallIdx <- smallIdx + 1
+                        mappedSmall <- map(x.[smallIdx])
+
+                    if mappedLarge = mappedP && not (largeIdx = pidx) then
+                        largeIdx <- largeIdx - 1
+                        mappedLarge <- map(x.[largeIdx])
+
+                    let temp = x.[smallIdx]
+                    x.[smallIdx] <- x.[largeIdx]
+                    x.[largeIdx] <- temp
+                    mappedSmall <- map(x.[smallIdx])
+                    mappedLarge <- map(x.[largeIdx])
+
+            k
+        /// The value pointed at pivot is changed such that its value would be as if the array is sorted.
+        /// All elements i' with an index less than pivot satisfy map(i') <= map(m).
+        /// All elements i'' with an index greater than pivot satisfy map(i'') >= map(m).
+        /// See https://en.cppreference.com/w/cpp/algorithm/nth_element
+        /// Implementation : https://archive.siam.org/meetings/analco04/abstracts/CMartinez.pdf
+        member x.PartialSortInPlace(map : ('T -> 'U)  when 'U : comparison, m : int) = 
+            assert (m < x.Length)
+            let pivot i j = (i+j)/2
+            let rec partialSort i j =
+                if i < j then
+                    let pidx = pivot i j
+                    let k = x.PartitionInPlace(map, i, j, pidx)
+                    partialSort i (k-1)
+                    if k < (m - 1) then partialSort (k+1) j
+                    ()
+            partialSort 0 x.Length
+            
+
+ 
