7dfc8b2480b7e0ee72732fd0281f29c33baadb11
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-04-22 17:58:21 +0200

    + tests

--- HenzaiFunc.Test/AABBPlaneTests.fs
+++ HenzaiFunc.Test/AABBPlaneTests.fs
@@ -69,7 +69,26 @@ let AABBIntersectingFrustumLeft() =
     let mutable leftPlane = Geometry.ExtractLeftPlane(&viewProjectionMatrix)
     let intersectionResult = AABBProc.PlaneIntersection(aabb,&leftPlane)
 
+    Assert.Equal(IntersectionResult.Intersecting, intersectionResult)
 
+[<Fact>]
+let AABBIntersectingFarFrustumLeft() =
+    let cameraOriginWS = Vector3(10.0f, 0.0f, 0.0f)
+    let lookAt = Vector3(0.0f, 0.0f, -1.0f)
+    let up = Vector3.UnitY
+    let fov = MathF.PI/4.0f;
+    let near = 0.01f
+    let far = 10.0f
+    let aspectRatio = 1.0f
+    let viewMatrix = Matrix4x4.CreateLookAt(cameraOriginWS, lookAt, up);
+    let perspectiveProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(fov, aspectRatio, near, far);
+    let mutable viewProjectionMatrix = viewMatrix*perspectiveProjMatrix
+    let pMin = Vector4(5.0f,0.0f,-1.0f, 1.0f)
+    let pMax = Vector4(15.0f,1.0f,-0.0f, 1.0f)
+    let aabb = AABB(pMin, pMax)
+    let mutable leftPlane = Geometry.ExtractLeftPlane(&viewProjectionMatrix)
+    let intersectionResult = AABBProc.PlaneIntersection(aabb,&leftPlane)
+    
     Assert.Equal(IntersectionResult.Intersecting, intersectionResult)
 
 
