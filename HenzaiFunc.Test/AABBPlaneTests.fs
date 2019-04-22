c82fa0c13a77c2c9b9243b078d1486ab860c70f4
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-04-22 17:49:55 +0200

    added aabb plane tests

--- HenzaiFunc.Test/AABBPlaneTests.fs
+++ HenzaiFunc.Test/AABBPlaneTests.fs
@@ -27,7 +27,52 @@ let AABBOutsideFrustumLeft() =
     let leftPlane = Geometry.ExtractLeftPlane(&viewProjectionMatrix, &plane)
     let intersectionResult = AABBProc.PlaneIntersection(aabb,&plane)
 
-   
+
     Assert.Equal(IntersectionResult.Outside, intersectionResult)
+
+[<Fact>]
+let AABBInsideFrustumLeft() =
+    let cameraOriginWS = Vector3(0.0f, 0.0f, 0.0f)
+    let lookAt = Vector3(0.0f, 0.0f, -1.0f)
+    let up = Vector3.UnitY
+    let fov = MathF.PI/4.0f;
+    let near = 0.01f
+    let far = 10.0f
+    let aspectRatio = 1.0f
+    let viewMatrix = Matrix4x4.CreateLookAt(cameraOriginWS, lookAt, up);
+    let perspectiveProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(fov, aspectRatio, near, far);
+    let mutable viewProjectionMatrix = viewMatrix*perspectiveProjMatrix
+    let pMin = Vector4(19.0f,0.0f,-1.0f, 1.0f)
+    let pMax = Vector4(20.0f,1.0f,-0.0f, 1.0f)
+    let aabb = AABB(pMin,pMax)
+    let mutable plane = Vector4.Zero
+    let leftPlane = Geometry.ExtractLeftPlane(&viewProjectionMatrix, &plane)
+    let intersectionResult = AABBProc.PlaneIntersection(aabb,&plane)
+
+
+    Assert.Equal(IntersectionResult.Inside, intersectionResult)
+
+
+[<Fact>]
+let AABBIntersectingFrustumLeft() =
+    let cameraOriginWS = Vector3(0.0f, 0.0f, 0.0f)
+    let lookAt = Vector3(0.0f, 0.0f, -1.0f)
+    let up = Vector3.UnitY
+    let fov = MathF.PI/4.0f;
+    let near = 0.01f
+    let far = 10.0f
+    let aspectRatio = 1.0f
+    let viewMatrix = Matrix4x4.CreateLookAt(cameraOriginWS, lookAt, up);
+    let perspectiveProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(fov, aspectRatio, near, far);
+    let mutable viewProjectionMatrix = viewMatrix*perspectiveProjMatrix
+    let pMin = Vector4(-5.0f,0.0f,-1.0f, 1.0f)
+    let pMax = Vector4(5.0f,1.0f,-0.0f, 1.0f)
+    let aabb = AABB(pMin, pMax)
+    let mutable plane = Vector4.Zero
+    let leftPlane = Geometry.ExtractLeftPlane(&viewProjectionMatrix, &plane)
+    let intersectionResult = AABBProc.PlaneIntersection(aabb,&plane)
+
+
+    Assert.Equal(IntersectionResult.Intersecting, intersectionResult)
 
 
