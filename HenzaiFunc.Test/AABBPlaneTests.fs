3f2f4235d6b05dbc534c893cba946d29848d6911
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-04-24 14:42:23 +0200

    Added new test for object space culling

--- HenzaiFunc.Test/AABBPlaneTests.fs
+++ HenzaiFunc.Test/AABBPlaneTests.fs
@@ -6,6 +6,7 @@ open Xunit
 open Henzai.Core.Acceleration
 open Henzai.Core.Numerics
 open Henzai.Core.Raytracing
+open Assimp
 
 
 [<Fact>]
@@ -38,17 +39,40 @@ let AABBInsideFrustumLeft() =
     let near = 0.01f
     let far = 10.0f
     let aspectRatio = 1.0f
-    let viewMatrix = Matrix4x4.CreateLookAt(cameraOriginWS, lookAt, up);
-    let perspectiveProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(fov, aspectRatio, near, far);
+    let viewMatrix = Matrix4x4.CreateLookAt(cameraOriginWS, lookAt, up)
+    let perspectiveProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(fov, aspectRatio, near, far)
     let mutable viewProjectionMatrix = viewMatrix*perspectiveProjMatrix
-    let pMin = Vector4(19.0f,0.0f,-1.0f, 1.0f)
-    let pMax = Vector4(20.0f,1.0f,-0.0f, 1.0f)
+    let pMin = Vector4(0.0f,0.0f,-1.0f, 1.0f)
+    let pMax = Vector4(1.0f,1.0f,0.0f, 1.0f)
     let aabb = AABB(pMin,pMax)
     let mutable leftPlane = Geometry.ExtractLeftPlane(&viewProjectionMatrix)
     let intersectionResult = AABBProc.PlaneIntersection(aabb,&leftPlane)
-
-
     Assert.Equal(IntersectionResult.Inside, intersectionResult)
+
+[<Fact>]
+let AABBInsideFrustumLeftObjectSpace() =
+    let cameraOriginWS = Vector3(0.0f, 0.0f, 0.0f)
+    let lookAt = Vector3(0.0f, 0.0f, -1.0f)
+    let up = Vector3.UnitY
+    let fov = MathF.PI/4.0f;
+    let near = 0.01f
+    let far = 10.0f
+    let aspectRatio = 1.0f
+    let translationMatrix = Matrix4x4.CreateTranslation(5.0f, 0.0f, 0.0f)
+    let viewMatrix = Matrix4x4.CreateLookAt(cameraOriginWS, lookAt, up)
+    let perspectiveProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(fov, aspectRatio, near, far)
+
+    let mutable viewProjectionMatrix = viewMatrix*perspectiveProjMatrix
+    let mutable worldViewProjectionMatrix = translationMatrix*viewMatrix*perspectiveProjMatrix
+    let pMin = Vector4(-5.0f,0.0f,-1.0f, 1.0f)
+    let pMax = Vector4(-4.0f,1.0f,0.0f, 1.0f)
+    let aabb = AABB(pMin,pMax)
+    let mutable leftPlaneObjSpace = Geometry.ExtractLeftPlane(&worldViewProjectionMatrix)
+    let mutable leftPlaneWorldSpace = Geometry.ExtractLeftPlane(&viewProjectionMatrix)
+    let intersectionResult1 = AABBProc.PlaneIntersection(aabb,&leftPlaneObjSpace)
+    let intersectionResult2 = AABBProc.PlaneIntersection(aabb,&leftPlaneWorldSpace)
+    Assert.Equal(IntersectionResult.Inside, intersectionResult1)
+    Assert.Equal(IntersectionResult.Outside, intersectionResult2)
 
 
 [<Fact>]
