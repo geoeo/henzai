ed3d9681fa612c5f0ee7e1770e41b91466efd0e9
Marc Haubenstock <>
2020-03-08 17:20:33 +0100

    win 10

--- Projects/lights/Program.cs
+++ Projects/lights/Program.cs
@@ -9,7 +9,7 @@ namespace Henzai.Examples
         static void Main(string[] args)
         {
             Program programm = new Program();
-            programm.createScene(GraphicsBackend.Metal);
+            programm.createScene(GraphicsBackend.OpenGL);
         }
 
         public override void createScene(GraphicsBackend graphicsBackend, Sdl2Window contextWindow = null){
